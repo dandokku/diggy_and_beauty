7b02f1883d1c3ea24b215ea64000c7d49feead77
Dandokku <jesulobadaniel1@gmail.com>
2025-03-04 00:41:45 +0100

    soifd

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,7 +3,7 @@ import ProductCard from "../components/ProductCard";
 
 const ProductPage = () => {
   return (
-    <div className="bg-lightbg min-h-screen pt-24">
+    <div className="bg-lightbg min-h-screen pt-24 mt-16">
       <div className="container mx-auto p-10">
         <h1 className="text-4xl font-bold text-center text-primary mb-10">Our Products</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
